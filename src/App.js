ed0ad7abd2510beb7877bc27f3ba5338815aeb9d
bhanu <bhanutejaswinikaluva@gmail.com>
2024-05-04 11:51:11 +0530

    Modifying fotter

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import UserDataForm from './components/UserDataForm'; // Import the UserDataForm
 import Footer from './components/Footer'; // Import the Footer component
 import './App.css'; // Import the CSS file
 import SecondRowComponent from './components/SecondRowComponent';
+import SmoothAnimation from './components/SmoothAnimation';
 import FluidAnimation from './components/FluidAnimation';
 import BezierCurveAnimation from './components/BezierCurveAnimation';
 import ReactSpringAnimation from './components/ReactSpringAnimation';
-import SmoothAnimation from './components/SmoothAnimation';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -22,44 +22,34 @@ const App = () => {
         <RichTextEditor />
       </div>
       <div className="form-container">
-      <div className="user-box">
-          <SecondRowComponent />
-        </div>
-        
-      </div>
-      <div className="form-box">
+        <div className="form-box">
           <UserDataForm />
         </div>
-      
-      {/* <Footer /> */}
+      </div>
+      <div className="user-container">
+        <div className="user-box">
+          <SecondRowComponent /> {/* Replaced user information box with text editor */}
+        </div>
+      </div>
 
-      <div className="footer-container">
-  <div className="animation-row">
-    <div className="smooth-animation">
-      <SmoothAnimation />
-    </div>
-  </div>
-  <div className="animation-row">
-    <div className="fluid-animation">
-      <FluidAnimation />
-    </div>
-  </div>
-  <div className="animation-row">
-    <div className="bezier-curve-animation">
-      <BezierCurveAnimation />
-    </div>
-  </div>
-  <div className="animation-row">
-    <div className="react-spring-animation">
-      <ReactSpringAnimation />
-    </div>
-  </div>
-</div>
-
-
-
-
-
+      <div className="fixed-footer">
+        <div className="box animation-box">
+          <SmoothAnimation />
+        </div>
+        <div className="box animation-box">
+          <FluidAnimation />
+        </div>
+        <div className="box animation-box">
+          <BezierCurveAnimation />
+        </div>
+        <div className="box animation-box">
+          <ReactSpringAnimation />
+        </div>
+        {/* <div className="box footer-box">
+          <Footer />
+        </div> */}
+      </div>
+  
     </div>
   );
 };
